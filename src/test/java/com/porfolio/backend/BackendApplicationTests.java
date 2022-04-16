e53b572b95b4c4b3bbafdb192f9eebc0dc115664
Yash Tibrewal <yashtibrewal@Yashs-MacBook-Air.local>
2022-04-16 21:39:55 +0530

    initial commit

---
+++ src/test/java/com/porfolio/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
